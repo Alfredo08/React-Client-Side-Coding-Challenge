17feda1b2417088034b1d358be2dbd93cdee0c0e
Alfredo Salazar <>
2020-09-01 09:21:06 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import Book from './Book';
+import BookForm from './BookForm';
+
+class App extends React.Component{
+
+  constructor( props ){
+    super( props );
+    this.state = {
+      /*
+        Your code goes here
+      */
+    }
+  }
+
+  /* 
+    Your code goes here
+  */
+
+  render(){
+    return(
+      <div>
+        {/* 
+          Your code goes here
+        */}
+      </div>
+    )
+  }
+
+}
+
+export default App;
---
+++ src/BookForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function BookForm( props ){
+    return(
+        <div>
+            {/*
+                Your code goes here
+            */}
+        </div>
+    );
+}
+
+export default BookForm;
